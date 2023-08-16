3c6b5aab1b6371fd0218d8dc4d5cd660d83063c1
Will Torres <w.torresjr@gmail.com>
2023-08-16 01:33:44 -0400

    spots.id

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -26,7 +26,7 @@ router.get("/", async (req, res) => {
           ],
         },
       ],
-      group: ["Spot.id"],
+      group: ["Spots.id"],
     });
     res.json({ Spots: allSpots });
   } catch (err) {
