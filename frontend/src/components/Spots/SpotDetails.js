5da68cf791b2923772305951a837199784035b89
Will Torres <w.torresjr@gmail.com>
2023-09-23 13:07:39 -0400

    Styling spot details page, refactoring to get one spot instead of finding from all spots in my state

--- frontend/src/components/Spots/SpotDetails.js
+++ frontend/src/components/Spots/SpotDetails.js
@@ -11,8 +11,9 @@ const SpotDetails = () => {
   useEffect(() => {
     dispatch(fetchAllSpots(id));
   }, [dispatch, id]);
+
   const thisSpot = useSelector((state) =>
-    state?.spots?.allspots?.Spots.find((spot) => spot.id == id)
+    state?.spots?.allspots?.Spots.find((spot) => spot.id === parseInt(id))
   );
 
   return (
@@ -20,10 +21,24 @@ const SpotDetails = () => {
       <h3>{thisSpot?.name}</h3>
       <span></span>
       <p>{`${thisSpot?.city}, ${thisSpot?.state}, ${thisSpot?.country}`}</p>
-      <img
-        src={thisSpot?.previewImage}
-        className="preview-image"
-      />
+      <img src={thisSpot?.previewImage} className="preview-image" />
+
+      <div className="underImgs">
+        <p className="spotDescription">
+          <h3>{`Hosted by`}</h3>
+          {thisSpot?.description}
+        </p>
+        <div className="reserveContainer">
+          <div className="priceNrating">
+            <h4>{`$${thisSpot?.price}/night`}</h4>
+            <p>
+              {thisSpot.avgStarRating || "New"}{" "}
+              {thisSpot.numReviews || "No reviews yet"}
+            </p>
+          </div>
+          <button id="reserveBtn">Reserve</button>
+        </div>
+      </div>
     </div>
   );
 };
